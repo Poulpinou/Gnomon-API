eafe35c2f2e51ae3c4525d278a7af9c35be59c73
Donovan Persent <donovan.persent@gmail.com>
2020-04-14 17:42:34 +0200

    Add authentication

---
+++ src/main/java/com/gnomon/api/payloads/requests/LoginRequest.java
@@ -0,0 +1,27 @@
+package com.gnomon.api.payloads.requests;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+    @NotBlank
+    private String nameOrEmail;
+
+    @NotBlank
+    private String password;
+
+    public String getNameOrEmail() {
+        return nameOrEmail;
+    }
+
+    public void setNameOrEmail(String nameOrEmail) {
+        this.nameOrEmail = nameOrEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
